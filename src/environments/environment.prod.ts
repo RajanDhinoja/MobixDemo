e7cfc4ce7ce6d1323306c118f556af21c11c4af8
RajanDhinoja <rajandhinoja@gmail.com>
2022-03-12 00:41:26 +0530

    added firebase config to the environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: 'AIzaSyBFrMwt_C6mnQfQy_N5pkX0NfhlHB5mK0k',
+    authDomain: 'tech-challenge-mobix.firebaseapp.com',
+    projectId: 'tech-challenge-mobix',
+    storageBucket: 'tech-challenge-mobix.appspot.com',
+    messagingSenderId: '565345440636',
+    appId: '1:565345440636:web:aece4e52edc561eaf18af9',
+    measurementId: 'G-L8W6NW25M2',
+  },
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig : {
+    apiKey: 'AIzaSyBFrMwt_C6mnQfQy_N5pkX0NfhlHB5mK0k',
+    authDomain: 'tech-challenge-mobix.firebaseapp.com',
+    projectId: 'tech-challenge-mobix',
+    storageBucket: 'tech-challenge-mobix.appspot.com',
+    messagingSenderId: '565345440636',
+    appId: '1:565345440636:web:aece4e52edc561eaf18af9',
+    measurementId: 'G-L8W6NW25M2'
+  }
 };
 
 /*
